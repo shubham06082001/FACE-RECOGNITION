239f049251860e33350b0034bf80287dba49e135
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2020-08-04 14:16:31 +0530

    Updates

---
+++ precache-manifest.9518804eb9123f6af09950307a2d123c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e451f55d9199b936b8c5b8f3887143a",
+    "url": "/FACE-RECOGNITION/index.html"
+  },
+  {
+    "revision": "184131f70952abe02853",
+    "url": "/FACE-RECOGNITION/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3a474710d758525e91ce",
+    "url": "/FACE-RECOGNITION/static/css/main.73f289b6.chunk.css"
+  },
+  {
+    "revision": "184131f70952abe02853",
+    "url": "/FACE-RECOGNITION/static/js/2.75dfc972.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FACE-RECOGNITION/static/js/2.75dfc972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a474710d758525e91ce",
+    "url": "/FACE-RECOGNITION/static/js/main.ed94e54d.chunk.js"
+  },
+  {
+    "revision": "e9fdedb5d2962200ef58",
+    "url": "/FACE-RECOGNITION/static/js/runtime-main.f1e4dca7.js"
+  }
+]);
